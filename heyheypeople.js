9d1c4d96815daa4bbb7e65c020324f4d9a80dde5
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2020-06-15 23:11:57 -0400

    Finish main menu page

---
+++ heyheypeople.js
@@ -0,0 +1,5 @@
+function displayMain(){
+    document.getElementById("main-page").style.display = "flex";
+    document.getElementById("main-menu").style.display = "none";
+    console.log("HELOl");
+}
