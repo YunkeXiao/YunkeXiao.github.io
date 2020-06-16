bb9877c904e19ee0f217e189dc0ca82d8a252136
Yunke Xiao <33202018+BrotherYunke@users.noreply.github.com>
2020-06-15 23:57:16 -0400

    Add transition to main page

--- heyheypeople.js
+++ heyheypeople.js
@@ -1,5 +1,12 @@
-function displayMain(){
-    document.getElementById("main-page").style.display = "flex";
-    document.getElementById("main-menu").style.display = "none";
-    console.log("HELOl");
+window.onload = () => {
+    let target = document.getElementById("visit-container");
+    target.addEventListener('click', transition, false);
+}
+
+let transition = () =>{
+    let menu = document.getElementById("main-menu");
+    let main = document.getElementById("main-page");
+    menu.style.opacity = '0';
+    menu.addEventListener('transitionend', () => menu.remove());
+    main.style.opacity = '100';
 }
