af000183a5dd35408e47c215cf40c689f3134d08
yunke <33202018+YunkeXiao@users.noreply.github.com>
2021-01-04 19:59:08 -0500

    Update heyheypeople.js

--- heyheypeople.js
+++ heyheypeople.js
@@ -7,6 +7,7 @@ let transition = () =>{
     let menu = document.getElementById("main-menu");
     let main = document.getElementById("main-page");
     menu.style.opacity = '0';
+    menu.style.display = "none";
     menu.addEventListener('transitioned', () => menu.remove());
     main.style.display = "flex";
     main.style.opacity = '100';
